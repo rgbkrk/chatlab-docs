fd0f330fa11ed49abb32c9b9b7b518f6bdc63b97
Kyle Kelley <rgbkrk@gmail.com>
2023-07-03 13:05:08 -0700

    create blocks for inputs and outputs

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,48 +1,23 @@
 import React from "react";
 import CodeBlock from "@theme-original/CodeBlock";
 
-const ExecutionCount = ({ count }) => {
-  // If count is not a number, return a [ ]
-  let counterText = "[ ]:";
-  if (typeof count === "number" || typeof count === "string") {
-    counterText = `[${count}]:`;
-  }
-  return (
-    <div
-      style={{
-        fontFamily:
-          'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-        color: "#a7a7a7",
-        letterSpacing: "normal",
-        lineHeight: "1rem",
-        fontSize: "1rem",
-        marginTop: "1rem",
-        marginRight: "1rem",
-        minWidth: "3rem",
-        textAlign: "right",
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        userSelect: "none",
-      }}
-    >
-      {counterText}
-    </div>
-  );
-};
+import { InputBlock } from "@site/src/components/cell";
 
 export default function CodeBlockWrapper(props) {
+  if (props.mediaType == "text/plain") {
+    console.log("We got em");
+    console.log(props);
+    // return <CodeBlock {...props} style={{ backgroundColor: "green" }} />;
+    return <pre>{props.children}</pre>;
+  }
+
   if (!props.cell) {
     return <CodeBlock {...props} />;
   }
 
-  console.log(props);
-
   return (
-    <div style={{ display: "flex", alignItems: "flex-start", padding: "1rem" }}>
-      <ExecutionCount count={props.executionCount} />
-      <div style={{ flexGrow: 0, overflow: "auto" }}>
-        <CodeBlock {...props} />
-      </div>
-    </div>
+    <InputBlock count={props.executionCount}>
+      <CodeBlock {...props} />
+    </InputBlock>
   );
 }
