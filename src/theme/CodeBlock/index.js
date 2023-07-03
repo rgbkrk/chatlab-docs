e3b42e98184995eaae0a852cff88f567b13b685c
Kyle Kelley <rgbkrk@gmail.com>
2023-07-03 13:10:37 -0700

    remove extraneous logging

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -5,9 +5,6 @@ import { InputBlock } from "@site/src/components/cell";
 
 export default function CodeBlockWrapper(props) {
   if (props.mediaType == "text/plain") {
-    console.log("We got em");
-    console.log(props);
-    // return <CodeBlock {...props} style={{ backgroundColor: "green" }} />;
     return <pre>{props.children}</pre>;
   }
 
